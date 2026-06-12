1710a7ed7d293ccad26b9afcc704e6b57c371b2e
maxwellfairman <maxwellafairman@gmail.com>
2026-06-11 21:34:42 -0400

    bass notes? 13

--- src/components/ReactPiano.jsx
+++ src/components/ReactPiano.jsx
@@ -121,7 +121,7 @@ export default function ReactPiano({ onChordUpdate, playRoot, impliedRoot }) {
           if (!playRootRef.current || root === undefined || !bass) return;
 
           const note = Tone.Frequency(root, "midi").toNote();
-          bass.triggerAttackRelease(note, "1");
+          bass.triggerAttackRelease(note, "0.7");
         })
         .catch(console.error);
     }, 80);
